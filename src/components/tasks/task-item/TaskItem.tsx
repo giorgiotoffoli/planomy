c5c3b20a949d6f932dbd62f741bebc0784e73a4a
Gio <giorgio.toffoli@proton.me>
2026-03-18 15:58:38 -0500

    Dates now marked red if past due

--- src/components/tasks/task-item/TaskItem.tsx
+++ src/components/tasks/task-item/TaskItem.tsx
@@ -3,9 +3,10 @@ import { TaskEditButton } from './task-edit/TaskEditButton'
 import { TaskEditDropdown } from './task-edit/TaskEditDropdown'
 import { TaskCheckbox } from './TaskCheckbox'
 import { TaskTitle } from './TaskTitle'
-import { format } from 'date-fns'
+import { format, isBefore, isToday, parseISO } from 'date-fns'
 
 export function TaskItem({ task }: { task: Task }) {
+  const today = new Date()
   return (
     <li
       className="flex justify-between items-center group 
@@ -18,7 +19,15 @@ export function TaskItem({ task }: { task: Task }) {
           <TaskTitle task={task} />
         </div>
         <span className="text-xs flex justify-start gap-2">
-          <span>{task.due_date && `${format(task.due_date, 'PPP')}`}</span>
+          {/* Date */}
+          {task.due_date && isBefore(task.due_date, today) ? (
+            <span className="text-rose-500">
+              {task.due_date && `${format(task.due_date, 'PPP')}`}
+            </span>
+          ) : (
+            <span>{task.due_date && `${format(task.due_date, 'PPP')}`}</span>
+          )}
+          {/* Notes */}
           <span>{task.notes && `${task.notes}`}</span>
         </span>
       </div>
