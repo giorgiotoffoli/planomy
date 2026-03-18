c88682d466d3326b50ae33c212e1ad1e0d8f85a8
Gio <giorgio.toffoli@proton.me>
2026-03-17 20:43:27 -0500

    More work on the add-task-button component, made it bigger for mobile

--- src/components/tasks/TaskList.tsx
+++ src/components/tasks/TaskList.tsx
@@ -9,7 +9,7 @@ export function TaskList({ tasks }: { tasks: Task[] }) {
       <CreateTaskDialog>
         <CreateTaskButton />
       </CreateTaskDialog>
-      <div className="flex-1 overflow-y-auto scroll-smooth pb-6">
+      <div className="flex-1 overflow-y-auto scroll-smooth pb-19">
         {tasks?.length ? (
           <ul>
             {tasks?.map((task) => (
