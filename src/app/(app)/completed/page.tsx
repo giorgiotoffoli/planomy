33056f767ca37681984d88532a8e8e7791753dea
Gio <giorgio.toffoli@proton.me>
2026-03-27 22:24:04 -0500

    Create /completed

--- src/app/(app)/completed/page.tsx
+++ src/app/(app)/completed/page.tsx
@@ -3,12 +3,12 @@ import { getTasks } from '../../queries'
 import { Suspense } from 'react'
 import Header from '@/components/layout/header/Header'
 
-export default async function AllPage() {
-  const tasks = await getTasks('all')
+export default async function CompletedPage() {
+  const tasks = await getTasks('completed')
 
   return (
     <>
-      <Header taskCount={tasks.length} headerTitle="All" />
+      <Header taskCount={tasks.length} headerTitle="Completed" />
       <Suspense fallback={<h1>Loading tasks...</h1>}>
         <TaskList tasks={tasks} />
       </Suspense>
