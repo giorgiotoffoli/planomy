b05c12f78d299177447d0fe712fecdeee6648e65
Gio <giorgio.toffoli@proton.me>
2026-08-17 11:54:15 -0500

    Change description

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,7 +20,7 @@ const geistMono = Geist_Mono({
 
 export const metadata: Metadata = {
   title: 'Planomy',
-  description: 'Plan privately',
+  description: 'The open-source planner for your private life.',
 }
 
 export default function RootLayout({
